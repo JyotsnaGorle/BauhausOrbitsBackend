4e03d5adb85e9ee3292529f41a9e1f03b1c213fc
Patrick Saad <patrick.saad@uni-weimar.de>
2019-02-11 17:16:30 +0100

    simple sentiment analysis

--- data-processing/process-data.py
+++ data-processing/process-data.py
@@ -6,13 +6,13 @@ import nltk
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import sent_tokenize 
 from nltk.tokenize import word_tokenize
+from textblob import TextBlob
 
 # from nltk.corpus import stopwords
 # stopwords.words('english')
 
 nltk.download('punkt')
 nltk.download('wordnet')
-
 
 # Setup neo4j driver once
 neo4j_args = {
@@ -26,7 +26,6 @@ neo4j_driver = GraphDatabase.driver(
 
 # load files
 data_dir = 'data'
-
 output_dir = "output"
 
 for filename in glob.glob(data_dir + '/*.txt'):
@@ -39,73 +38,52 @@ for filename in glob.glob(data_dir + '/*.txt'):
         file_content = re.sub('\s+', ' ', file_content).strip()
         sentences = sent_tokenize(file_content)
         
+        print("Processing sentences...")
+        total_sentences = len(sentences)
+        counter = 0
+
+        c = 0
         for sentence in sentences:
+            print(str(c) + "/" + str(total_sentences))
             words = word_tokenize(sentence)
             has_verb = False
 
+            # Wword count cutoff
             if len(words) > 12 and len([x for x in ["INDEX", "APPENDIX"] if x in words]) == 0:
                 for w in words:
                     pos_l = set()
                     for tmp in wn.synsets(w):
+                        # At least a verb in the sentence
                         if tmp.pos() == 'v':
                             has_verb = True
                             break
             if has_verb:
-                final_sentences.append(sentence)
+                
+                # Sentiment Analysis
+                sentiment_score = round(TextBlob(sentence).sentiment.polarity, 3)
+                sentiment_label = 'positive' if sentiment_score > 0 \
+                                             else 'negative' if sentiment_score < 0 \
+                                                 else 'neutral'
+                final_sentences.append({
+                    "sentiment": sentiment_label,
+                    "sentence": sentence
+                })
+
+                counter += 1
+
+            c +=1
     file.close()
 
+    # Write output
+    output_path = output_dir + "/" + book_title + "/sentences.txt"
+    print("Writing output to: " + str(output_path))
 
     if not os.path.exists(output_dir + "/" + book_title):
         os.makedirs(output_dir + "/" + book_title)
-
-    with open(output_dir + "/" + book_title + "/sentences.txt", 'w+', encoding='utf-8') as file_sentences:
-        for sentence in final_sentences:
-            file_sentences.write(sentence + "\n")
+    with open(output_path, 'w+', encoding='utf-8') as file_sentences:
+        for row in final_sentences:
+            file_sentences.write(row["sentiment"] + "," + row["sentence"] + "\n")
     file_sentences.close()
-
-        # file_lines = file.readline()
-        # total_lines = len(file_lines)
-
-        # # Parse
-        # book_author = filename.replace(data_dir + "/", "").split("_")[0].replace("-"," ").title()
-
-        # # find pages, everything before a page is the content of that page
-        # # pages = file_content.
-
-        # page_texts = {}
-        # page_text = ""
-
-        # c = 1
-        # for line in file_lines:
-        #   line = line.replace("\n", " ").strip()
-
-        #   check_integer = False
-        #   try:
-        #       check_integer = int(line)
-        #   except:
-        #       pass
-
-        #   if check_integer:
-
-        #       # add the page's text
-        #       page_texts[line] = page_text
-
-        #       # reset
-        #       page_text = ""
-
-        #   elif(c != total_lines):
-        #       page_text += line
-
-        #   c += 1
-
-
-        # for k,v in page_texts.items():
-        #   print(k+"test")
-        #   print(v)
-        #   print("\n\n")
-
-
-
 
 # Close neo4j driver
 neo4j_driver.close()
