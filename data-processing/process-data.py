d444d651b67ba21357d5bb4782b28c6c3023faf6
Patrick Saad <patrick.saad@uni-weimar.de>
2019-02-18 15:42:00 +0100

    books text files parsing and save to neo4j

--- data-processing/process-data.py
+++ data-processing/process-data.py
@@ -14,76 +14,125 @@ from textblob import TextBlob
 nltk.download('punkt')
 nltk.download('wordnet')
 
-# Setup neo4j driver once
-neo4j_args = {
-    "uri" : "bolt://localhost:7688",
-    "user" : "neo4j",
-    "password" : "bauhaus"
-}
-neo4j_driver = GraphDatabase.driver(
-    neo4j_args['uri'], auth = (neo4j_args['user'],neo4j_args['password'])
-)
+def add_book_sentence(tx, book, sentence):
+    tx.run(
+    "MERGE (b:Book {book_id: $b_id}) " +
+    "SET b.author = $b_author, b.title = $b_title " + 
+    "MERGE (s:Sentence {sentence_id: $s_id}) " + 
+    "SET s.text = $s_text, s.page = $s_page, s.sentiment_score = $s_sentiment_score " +
+    "MERGE (s)-[:IS_IN]->(b)", b_id=book['id'], b_author=book["author"], b_title=book["title"], s_id=sentence['id'], s_text=sentence['text'], s_page=sentence["page"], s_sentiment_score=sentence['sentiment_score'])
 
-# load files
-data_dir = 'data'
-output_dir = "output"
+def run():
+    # Setup neo4j driver once
+    neo4j_args = {
+        "uri" : "bolt://localhost:7688",
+        "user" : "neo4j",
+        "password" : "bauhaus"
+    }
+    neo4j_driver = GraphDatabase.driver(
+        neo4j_args['uri'], auth = (neo4j_args['user'],neo4j_args['password'])
+    )
 
-for filename in glob.glob(data_dir + '/*.txt'):
+    with neo4j_driver.session() as session:
 
-    book_title = filename.replace(data_dir + "/", "").split("_")[1].split(".")[0]
-
-    final_sentences = []
-    with open(filename, 'r', encoding='utf-8') as file:
-        file_content = file.read()
-        file_content = re.sub('\s+', ' ', file_content).strip()
-        sentences = sent_tokenize(file_content)
+        # Book "id" field is a unique key
+        c_p = session.run("CREATE CONSTRAINT ON (b:Book) ASSERT b.book_id IS UNIQUE")
         
-        print("Processing sentences...")
-        total_sentences = len(sentences)
-        counter = 0
+        # Sentence "ids" field is a unique key
+        c_a = session.run("CREATE CONSTRAINT ON (s:Sentence) ASSERT s.sentence_id IS UNIQUE")
 
-        c = 0
-        for sentence in sentences:
-            print(str(c) + "/" + str(total_sentences))
-            words = word_tokenize(sentence)
-            has_verb = False
+        # load files
+        data_dir = "data"
+        output_dir = "output"
 
-            # Wword count cutoff
-            if len(words) > 12 and len([x for x in ["INDEX", "APPENDIX"] if x in words]) == 0:
-                for w in words:
-                    pos_l = set()
-                    for tmp in wn.synsets(w):
-                        # At least a verb in the sentence
-                        if tmp.pos() == 'v':
-                            has_verb = True
-                            break
-            if has_verb:
-                
-                # Sentiment Analysis
-                sentiment_score = round(TextBlob(sentence).sentiment.polarity, 3)
-                sentiment_label = 'positive' if sentiment_score > 0 \
-                                             else 'negative' if sentiment_score < 0 \
-                                                 else 'neutral'
-                final_sentences.append({
-                    "sentiment": sentiment_label,
-                    "sentence": sentence
-                })
+        book_counter = 0
 
-                counter += 1
+        for subdir, book_dir, book_files in os.walk(data_dir):
 
-            c +=1
-    file.close()
+            if not book_dir:
+                continue
 
-    # Write output
-    output_path = output_dir + "/" + book_title + "/sentences.txt"
-    print("Writing output to: " + str(output_path))
+            book_counter += 1
 
-    if not os.path.exists(output_dir + "/" + book_title):
-        os.makedirs(output_dir + "/" + book_title)
-    with open(output_path, 'w+', encoding='utf-8') as file_sentences:
-        for row in final_sentences:
-            file_sentences.write(row["sentiment"] + "," + row["sentence"] + "\n")
-    file_sentences.close()
+            book_meta = book_dir[0].split("_")
+            book_author = book_meta[0].replace("-", " ")
+            book_title = book_meta[1]
 
-# Close neo4j driver
-neo4j_driver.close()
+            print(book_title)
+
+            book = {
+                "id": book_counter,
+                "title": book_title,
+                "author": book_author
+            }
+
+            # Save book
+            # session.write_transaction(add_book, book)
+            sentence_counter = 1
+
+            for filename in glob.glob(data_dir + '/' + book_dir[0] + '/*.txt'):
+
+                current_page = filename.split(" ")[-1].replace(".pdf.txt", "").replace(book_title, "")
+                print("......... Page: " + str(current_page))
+
+                final_sentences = []
+                with open(filename, 'r', encoding="latin-1") as file:
+                    file_content = file.read()
+                    file_content = re.sub('\s+', ' ', file_content).strip()
+                    sentences = sent_tokenize(file_content)
+                    
+                    total_sentences = len(sentences)
+
+                    c = 0
+                    for sentence in sentences:
+                        words = word_tokenize(sentence)
+                        has_verb = False
+
+                        # Wword count cutoff
+                        if len(words) > 12 and len([x for x in ["INDEX", "APPENDIX"] if x in words]) == 0:
+                            for w in words:
+                                pos_l = set()
+                                for tmp in wn.synsets(w):
+                                    # At least a verb in the sentence
+                                    if tmp.pos() == 'v':
+                                        has_verb = True
+                                        break
+                        if has_verb:
+                            
+                            # Sentiment Analysis
+                            sentiment_score = round(TextBlob(sentence).sentiment.polarity, 3)
+                            sentiment_label = 'positive' if sentiment_score > 0 \
+                                                         else 'negative' if sentiment_score < 0 \
+                                                             else 'neutral'
+
+                            sentence_dict = {
+                                "id": sentence_counter,
+                                "text": sentence,
+                                "sentiment_score": sentiment_score,
+                                "page": current_page
+                            }
+
+                            # Save sentence
+                            session.write_transaction(add_book_sentence, book, sentence_dict)
+
+                            sentence_counter += 1
+
+                        c +=1
+                print("................. " + str(sentence_counter) + " sentences added")
+                file.close()
+
+                # Write output
+                # output_path = output_dir + "/" + book_dir[0]
+                # print("Writing output to: " + str(output_path))
+
+                # if not os.path.exists(output_path):
+                #     os.makedirs(output_path)
+                # with open(output_path + "/" + str(current_page) + ".txt", 'w+', encoding='utf-8') as file_sentences:
+                #     for row in final_sentences:
+                #         file_sentences.write(row["sentiment"] + "," + row["sentence"] + "\n")
+                # file_sentences.close()
+
+    # Close neo4j driver
+    neo4j_driver.close()
+
+run()
