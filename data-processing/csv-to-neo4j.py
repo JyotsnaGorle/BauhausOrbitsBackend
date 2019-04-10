08936d15628e2438e6e81e95db7adac012c93466
Patrick Saad <patrick.saad@uni-weimar.de>
2019-04-10 16:53:59 +0200

    clean parse, lda, new db

--- data-processing/csv-to-neo4j.py
+++ data-processing/csv-to-neo4j.py
@@ -7,7 +7,7 @@ def add_row(tx, row):
         'MERGE (b:Book {book_id: $b_id}) ' +
         'SET b.series_number = $b_series_number, b.color = $b_color, b.author = $b_author, b.title = $b_title, b.year = $b_year ' + 
         'MERGE (s:Sentence {sentence_id: $s_id}) ' + 
-        'SET s.sentence_number = $s_number, s.color = $s_color, s.text = $s_text, s.page = $s_page, s.sentiment_score = $s_sentiment_score ' +
+        'SET s.type = $s_type, s.sentence_number = $s_number, s.color = $s_color, s.text = $s_text, s.page = $s_page, s.sentiment_score = $s_sentiment_score ' +
         'MERGE (s)-[:IS_IN]->(b)',
         b_color = '#ff0000',
         b_series_number = row['book_series_number'],
@@ -17,11 +17,36 @@ def add_row(tx, row):
         b_year = row['book_year'],
         s_color = '#fbfbfb',
         s_page = row['sentence_page'],
+        s_type = row['sentence_type'],
         s_id = row['sentence_id'],
         s_number = row['sentence_number'],
         s_text = row['sentence_text'],
         s_sentiment_score = row['sentence_sentiment_score']
     )
+
+    # LDA
+    lda_topics = eval(row["sentence_lda"])
+    lda_topic_scores = eval(row["sentence_lda_scores"])
+
+    if len(lda_topics) == len(lda_topic_scores):
+        c = 0
+        for topic_word in lda_topics:
+
+            # score
+            topic_score = lda_topic_scores[c]
+
+            tx.run(
+                'MERGE (t:Topic {word: $word}) ' +
+                'SET t.score = $score ' +
+                'MERGE (s:Sentence {sentence_id: $s_id}) ' + 
+                'MERGE (t)-[:IS_IN]->(s)',
+                word = topic_word,
+                score = topic_score,
+                s_id = row['sentence_id']
+            )
+
+            c += 1
+
 
 def run():
     # Setup neo4j driver once
@@ -46,6 +71,9 @@ def run():
         # Sentence 'ids' field is a unique key
         c_a = session.run('CREATE CONSTRAINT ON (s:Sentence) ASSERT s.sentence_id IS UNIQUE')
 
+        # Topic's word is a unique field
+        c_a = session.run('CREATE CONSTRAINT ON (t:Topic) ASSERT t.word IS UNIQUE')
+
         with open(input_file, 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
 
@@ -59,6 +87,7 @@ def run():
 
                 # Save row
                 session.write_transaction(add_row, row)
+
                 c += 1
 
         query_float = 'MATCH (s:Sentence) SET s.sentiment_score = toFloat(s.sentiment_score)'
--- data-processing/parse-books-to-csv.py
+++ data-processing/parse-books-to-csv.py
@@ -7,9 +7,13 @@ from nltk.corpus import wordnet as wn
 from nltk.tokenize import sent_tokenize 
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
+from unidecode import unidecode
 
 nltk.download('punkt')
 nltk.download('wordnet')
+
+def text_to_unicode(text):
+    return unicode(text, encoding = "utf-8")
 
 def run():
     # load files
@@ -20,10 +24,11 @@ def run():
 
     book_counter = 0
 
-    all_rows = []
+    all_sentences = []
     books = os.listdir(data_dir)
 
     global_sentence_counter = 1
+
     for book in books:
 
         book_counter += 1
@@ -32,11 +37,13 @@ def run():
         book_meta = book.split("_")
         book_series_number = book_meta[0]
         book_author = book_meta[1].replace("-", " ").title()
-        book_title = ' '.join(book_meta[5:])
+        book_title = ' '.join(book_meta[5:-1])
+        book_language = book_meta[ len(book_meta)-1 ]
         book_year = book_meta[3]
 
         book_dict = {
             "id": book_counter,
+            "language": book_language,
             "series_number": book_series_number,
             "title": book_title,
             "author": book_author,
@@ -53,11 +60,13 @@ def run():
 
             # print("......... Page: " + str(current_page))
 
-            with open(filename, 'r', encoding="latin-1") as file:
+            with open(filename, 'r', encoding="utf-8") as file:
                 file_content = file.read()
-                file_content = re.sub('\s+', ' ', file_content).strip()
-
-                file_content = file_content.replace("\t", " ").strip()
+                file_content = file_content.replace("\t", " ")
+                file_content = file_content.replace("\n", " ")
+                file_content = file_content.replace("\r", " ")
+                file_content = file_content.replace("\n\r", " ")
+                file_content = re.sub(r'\s+', ' ', file_content).strip()
 
                 sentences = sent_tokenize(file_content)
                 total_sentences = len(sentences)
@@ -68,7 +77,7 @@ def run():
                     has_verb = False
 
                     # Word count cutoff
-                    if len(words) > 12 and len([x for x in ["INDEX", "APPENDIX"] if x in words]) == 0:
+                    if len(words) > 10 and len([x for x in ["INDEX", "APPENDIX"] if x in words]) == 0:
                         for w in words:
                             pos_l = set()
                             for tmp in wn.synsets(w):
@@ -77,23 +86,31 @@ def run():
                                     has_verb = True
                                     break
                     if has_verb:
-                        
                         # Sentiment Analysis
                         sentiment_score = round(TextBlob(sentence).sentiment.polarity, 3)
 
                         sentence_dict = {
                             "id": global_sentence_counter,
+                            "language": book_language,
+                            "type": "text",
                             "number": sentence_counter,
                             "text": sentence,
                             "sentiment_score": sentiment_score,
                             "page": current_page
                         }
 
+                        ##################################
+                        # Is this sentence a question?
+                        ##################################
+                        if sentence.endswith("?"):
+                            sentence_dict["type"] = "question"
+
+                        # add sentence
                         row = {
                             "book": book_dict,
                             "sentence": sentence_dict
                         }
-                        all_rows.append(row)
+                        all_sentences.append(row)
 
                         sentence_counter += 1
                         global_sentence_counter += 1
@@ -111,36 +128,40 @@ def run():
     with open(output_path, 'w', newline='') as csvfile:
         columns = [
             'book_id',
+            'book_language',
             'book_series_number',
             'book_title',
             'book_author',
             'book_year',
             'sentence_page',
             'sentence_id',
+            'sentence_language',
+            'sentence_type',
             'sentence_number',
             'sentence_text',
-            'sentence_sentiment_score',
-            'sentence_lda'
+            'sentence_sentiment_score'
         ]
         writer = csv.DictWriter(csvfile, fieldnames=columns)
         writer.writeheader()
 
-        for row in all_rows:
+        for row in all_sentences:
             book = row['book']
             sentence = row['sentence']
 
             writer.writerow({
                 'book_id' : book['id'],
+                'book_language' : book['language'],
                 'book_series_number' : book['series_number'],
                 'book_title' : book['title'],
                 'book_author' : book['author'],
                 'book_year' : book['year'],
-                'sentence_page' : sentence['page'],
                 'sentence_id' : sentence['id'],
+                'sentence_language' : sentence['language'],
+                'sentence_page' : sentence['page'],
+                'sentence_type' : sentence['type'],
                 'sentence_number' : sentence['number'],
                 'sentence_text' : sentence['text'],
-                'sentence_sentiment_score' : sentence['sentiment_score'],
-                'sentence_lda': ''
+                'sentence_sentiment_score' : sentence['sentiment_score']
             })
 
     csvfile.close()
