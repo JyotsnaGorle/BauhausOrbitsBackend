fdb61547b8d727936ee114067d945c8d3b688656
Patrick Saad <patrick.saad@uni-weimar.de>
2019-06-21 17:42:49 +0200

    speech to text

--- data-processing/audio-to-text.py
+++ data-processing/audio-to-text.py
@@ -1,90 +1,64 @@
+import os
+import glob
+from os import path
 import speech_recognition as sr
 from pydub import AudioSegment
 
-# obtain path to "english.wav" in the same folder as this script
-from os import path
-AUDIO_FILE = "sounds/Bauhaus_01-Phantasie-Op-17.mp3"
-sound = AudioSegment.from_mp3(AUDIO_FILE)
-sound.export("sounds/Bauhaus_01-Phantasie-Op-17.wav", format="wav")
+GOOGLE_SPEECH_KEY = "AIzaSyC0T0dWnAqW6AEoYw0aeXV7U5ZHKpTGyME"
 
-AUDIO_FILE = "sounds/Bauhaus_01-Phantasie-Op-17.wav"
+for audio_file_name in glob.glob('sounds/*.mp3'):
 
-# use the audio file as the audio source
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)  # read the entire audio file
+    sound = AudioSegment.from_mp3(audio_file_name)
+    
+    new_audio_file_name = audio_file_name.replace(".mp3", ".wav")
+    
+    if not os.path.exists(new_audio_file_name):
+        sound.export(new_audio_file_name, format="wav")
 
-    # recognize speech using Sphinx
-    try:
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    
+    with sr.AudioFile(new_audio_file_name) as source:
+        audio = r.record(source)
 
-	# recognize speech using Google Speech Recognition
-	# try:
-	#     # for testing purposes, we're just using the default API key
-	#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-	#     # instead of `r.recognize_google(audio)`
-	#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-	# except sr.UnknownValueError:
-	#     print("Google Speech Recognition could not understand audio")
-	# except sr.RequestError as e:
-	#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # recognize speech using Sphinx
+        # try:
+            # print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        # except sr.UnknownValueError:
+            # print("Sphinx could not understand audio")
+        # except sr.RequestError as e:
+            # print("Sphinx error; {0}".format(e))
 
-# # recognize speech using Google Cloud Speech
-# GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-# try:
-#     print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-# except sr.UnknownValueError:
-#     print("Google Cloud Speech could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
-# # recognize speech using Wit.ai
-# WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-# try:
-#     print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-# except sr.UnknownValueError:
-#     print("Wit.ai could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Wit.ai service; {0}".format(e))
-
-# # recognize speech using Microsoft Azure Speech
-# AZURE_SPEECH_KEY = "INSERT AZURE SPEECH API KEY HERE"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
-# try:
-#     print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
-# except sr.UnknownValueError:
-#     print("Microsoft Azure Speech could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
-
-# # recognize speech using Microsoft Bing Voice Recognition
-# BING_KEY = "INSERT BING API KEY HERE"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-# try:
-#     print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-# except sr.UnknownValueError:
-#     print("Microsoft Bing Voice Recognition could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# # recognize speech using Houndify
-# HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
-# HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
-# try:
-#     print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-# except sr.UnknownValueError:
-#     print("Houndify could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Houndify service; {0}".format(e))
-
-# # recognize speech using IBM Speech to Text
-# IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-# IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-# try:
-#     print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-# except sr.UnknownValueError:
-#     print("IBM Speech to Text could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from IBM Speech to Text service; {0}".format(e))
+        # recognize speech using Google Speech Recognition
+        # try:
+            # print("Google Speech Recognition thinks you said " + r.recognize_google(audio, key="AIzaSyC0T0dWnAqW6AEoYw0aeXV7U5ZHKpTGyME"))
+        # except sr.UnknownValueError:
+            # print("Google Speech Recognition could not understand audio")
+        # except sr.RequestError as e:
+            # print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            
+        # recognize speech using Google Cloud Speech
+        GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{
+              "type": "service_account",
+              "project_id": "arcane-geode-244410",
+              "private_key_id": "9ac9dc2c39d35f136751b330b41309cdead95b5a",
+              "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1n7hrvQ7ornJr\nSzZRTehRKc6xax9zdjz3F7BhNOJVLeaRyw19VZ5+1rTAO2Htea+Itoj5TSeXhO6a\ni0FY0aeEk/3ThNskyE4leXlUhZm+wVNk6muSHR+Nt8BzdeZRlv9lfJJ1XKy21Z3i\nALSo+1P7WWfDOTxeHr6CAMKxUITF9V3g0i+Jlh/5q7NRh0u4hBE9e29XTjPLAkHO\n1XZAZF0zdSrUaU5qjKZSaB0VdLW3/la8fqAR/8xhkuux/gqtMSxoNtQyA2XznsT4\n/Z0D6NLZ/KyMWYqUFcr83ud4voTJt5fTKY5bbl0SEptg6dWuqNW4o00rolZgn6Nm\nqBQMM8KDAgMBAAECggEAFefoRfJdGHpl6YP+TqsfYJ6yw9pgRjxWGRVd3DOVMuHg\n4+fCPkIz5id+UIs5AfwBzHL7rVn9cvyBDNnL7WsIEekJndfx2ItiRYnTtPqSMxw9\nMzhE9jAWDLFDgImRWHjmX/a48XfWiwzTyJz+LT13ASNk7Pp7yfxaTPLLiLhl8zwz\nWnrOqGCQh6zCpZOBoyOlk6rNmpVgVd6MWNKK7gD4qE0wEYplwo3ifWNR4AKH0SBq\n2SEmc7mTM0kK2N/Wx1Zf7IqqJFloCl2fndVmwNH2wJ7UXfCQQx8rSbtMTDcqNaAb\nArfYEh0iemkRZbOjPE2pDdlHeNrGrCMNlifI6hLp1QKBgQDjUxcKeKXa4JihxOQA\nX08YZ0Bb1HjRscRHhd7q0p1hB4jXp1QdsRNOurbkQw96xDV19l7ejr7BXhgdCb+g\nUi0Mdydkh36ZBmYLekGl1aBK17hjTLnRxAYbT+E40pqUO4jiEodNkOnMWBHEvuA7\nylHIan7yxy6OmvXYBFELQqlKDQKBgQDMiNXgIBJ0/XB2DOJHhaofpFv45JfZ6860\n2jAvRW1AsVxrUBwo7+UfllaUWhkxLsDMYpFRIpyNx3F5wiWmozNu4wBrRvoXmfNF\npkcYe7Zy+JSa96nBVzJXw/dqGi7OzJTN6ymBpYq3P74H8vRPGNNv+IRmwn61wHU5\nSAevUuXqzwKBgQC/zyJH4ZkAYDYpeTpw3fqdOrHhShxPw1E8kEaRmPjWIEdkv3mI\nPBtweLdNTxBGpDv/tsxBuexWuJBcIBeirPP56qhAeNtnPzDnSlcOGHzR6cdZcinw\nRxhn/zSof3uSy9EqADxORJIhq8YCXpRRntr24nUPYOV1ymbZyHqhp7O0GQKBgEMc\noxDndSbuAfi6QYU2VpwLUiJY33qh5TFyZl0carzPdYPAxXypOoUiz0XeSmXY8Woe\n7Y+xy6y5xIIvMit7YWUBFyIcJ/OWkDxKUwm1jigraJdwrELEWcByjLxD2xgACNOI\nVhY0O0/lmnUBQyiw/K/A27DcILeCbvRcrijZCpgBAoGAHlbnMNa0OhM0cBeDZmLM\n6zGJCSYbn/ZaToLfoy5P+l5QMDz0MGuOhnQ/UQKgFCegZsIKHFKVf6GFqQgw5+TW\nP8Gc6jboor1pMWzCYtL+tj1/0GKulFVavqoP/cE1I3vw97si94WLJqSnuNO74syJ\nSK5AMs8GnVtCW4Qrknvbwsk=\n-----END PRIVATE KEY-----\n",
+              "client_email": "testing@arcane-geode-244410.iam.gserviceaccount.com",
+              "client_id": "117003047473942590976",
+              "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+              "token_uri": "https://oauth2.googleapis.com/token",
+              "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+              "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testing%40arcane-geode-244410.iam.gserviceaccount.com"
+            }
+            """
+        try:
+            translation = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+            
+            # save it, or whatever
+            
+        except sr.UnknownValueError:
+            print("Google Cloud Speech could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Cloud Speech service; {0}".format(e))
+            
+            
