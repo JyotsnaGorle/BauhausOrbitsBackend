de82dcb2235d0a2c63d6cb92e8a2c25a047db525
jyotsnagorle <jyotsna.gorle@enginsight.com>
2019-06-24 17:04:25 +0200

    add endpoints for first 10 usecases

--- server.py
+++ server.py
@@ -42,20 +42,147 @@ class DBHandler(object):
     def close(self):
         self._driver.close()
 
-    def print_greeting(self, message):
+    def do_transaction(self):
         with self._driver.session() as session:
-            return session.write_transaction(self._create_and_return_greeting, message)
+            return session.write_transaction(self._query_for_books)
 
     @staticmethod
-    def _create_and_return_greeting(tx, message):
+    def _query_for_books(tx):
         result = tx.run(
-            "MATCH (s:Sentence) "
-            "WHERE s.sentiment_score = 1 "
-            "RETURN s",
+            "MATCH (n:Book)"
+            "RETURN n",
         )
         records = []
         for record in result:
+            records.append(dict(record['n'].items()))
+        return jsonify(records)
+
+    def get_all_pages(self, bookId):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_pages, bookId)
+
+    @staticmethod
+    def _query_for_pages(tx, bookId):
+        result = tx.run(
+            "MATCH (n:Book {book_id :{bookId}})<-[:IS_IN]-(s:Sentence) WITH toInteger(s.page) AS page_number RETURN DISTINCT(page_number) ORDER by page_number ASC",
+        bookId = bookId)
+        records = []
+        for record in result:
+            records.append(dict(record['page_number'].items()))
+        return jsonify(records)
+
+    def get_Top_Ten_Topics(self, bookId1, bookId2):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_topics, bookId1, bookId2)
+
+    @staticmethod
+    def _query_for_topics(tx, bookId1, bookId2):
+        result = tx.run(
+            "MATCH (b:Book)<-[r1:IS_IN]-(s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE b.book_id = {first} or b.book_id = {second} RETURN t LIMIT 10",
+        first = int(bookId1),
+        second = int(bookId2))
+        records = []
+        for record in result:
+            records.append(dict(record['t'].items()))
+        return jsonify(records)
+
+    def get_common_Topics(self, bookId1, bookId2):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_common_topics, bookId1, bookId2)
+
+    @staticmethod
+    def _query_for_common_topics(tx, bookId1, bookId2):
+        result = tx.run(
+            "MATCH (b:Book)<-[r1:IS_IN]-(s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE b.book_id = {first} or b.book_id = {second} RETURN t ORDER by t.score DESC LIMIT 10",
+        first = int(bookId1),
+        second = int(bookId2))
+        records = []
+        for record in result:
+            records.append(dict(record['t'].items()))
+        return jsonify(records)
+
+    def get_sentences_for_topic(self, topic):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_sentences_with_topic, topic)
+
+    @staticmethod
+    def _query_for_sentences_with_topic(tx, topic):
+        result = tx.run(
+            "MATCH (s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE t.word = {word} RETURN s",
+        word = topic)
+        records = []
+        for record in result:
             records.append(dict(record['s'].items()))
+        return jsonify(records)
+
+    def get_sentences_for_topic_sort_year(self, topic):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_sentences_with_topic_sort_year, topic)
+
+    @staticmethod
+    def _query_for_sentences_with_topic_sort_year(tx, topic):
+        result = tx.run(
+            "MATCH (n:Book)<-[r1:IS_IN]-(s:Sentence)<-[r2:IS_IN]-(t:Topic) WITH n.year AS book_year, s WHERE t.word = {word} RETURN s ORDER BY book_year DESC",
+        word = topic)
+        records = []
+        for record in result:
+            records.append(dict(record['s'].items()))
+        return jsonify(records)
+
+    def get_similar_topic_sentences(self, topic):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_sentences_with_topic_similarity, topic)
+
+    @staticmethod
+    def _query_for_sentences_with_topic_similarity(tx, topic):
+        result = tx.run(
+            "MATCH (s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE t.word = {word} RETURN s, t ORDER BY t.score ASC",
+        word = topic)
+        records = []
+        for record in result:
+            records.append(dict(record['s'].items()))
+        return jsonify(records)
+
+    def get_positive_topic_sentences(self, topic):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_sentences_with_positive_topic_sentiment, topic)
+
+    @staticmethod
+    def _query_for_sentences_with_positive_topic_sentiment(tx, topic):
+        result = tx.run(
+            "MATCH (s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE t.word = {word} RETURN s, t ORDER BY s.sentiment_score DSC",
+        word = topic)
+        records = []
+        for record in result:
+            records.append(dict(record['s'].items()))
+        return jsonify(records)
+
+    def get_negative_topic_sentences(self, topic):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_sentences_with_negative_topic_sentiment, topic)
+
+    @staticmethod
+    def _query_for_sentences_with_negative_topic_sentiment(tx, topic):
+        result = tx.run(
+            "MATCH (s:Sentence)<-[r2:IS_IN]-(t:Topic) WHERE t.word = {word} RETURN s, t ORDER BY s.sentiment_score ASC",
+        word = topic)
+        records = []
+        for record in result:
+            records.append(dict(record['s'].items()))
+        return jsonify(records)
+
+    def get_book_details(self, sentenceId):
+        with self._driver.session() as session:
+            return session.write_transaction(self._query_for_book_details, sentenceId)
+
+    @staticmethod
+    def _query_for_book_details(tx, sentenceId):
+        result = tx.run(
+            "MATCH (n:Book)<-[r1:IS_IN]-(s:Sentence {sentence_id: {sentenceId}}) RETURN n, s",
+        sentenceId = int(sentenceId))
+        records = []
+        for record in result:
+            records.append(dict(record['n'].items()))
         return jsonify(records)
 
 
@@ -67,20 +194,81 @@ def hello1():
     return jsonify({"messages": "response"})
 
 
-@app.route("/add", methods=["POST"])
+@app.route("/add/", methods=["POST"])
 def hello2():
     data = json.loads(request.data)
     return jsonify({"messages": "response"})
 
-
-@app.route("/echo/<msg>")
+@app.route("/all/")
 @requires_auth
-def hello(msg=None):
-    if not msg:
-        msg = "helooo"
-
-    response = obj.print_greeting(msg)
+def allBooks():
+    response = obj.do_transaction()
     return response
 
+@app.route("/allPagesForBook/<bookId>")
+@requires_auth
+def allPages(bookId=None):
+    if not bookId:
+        bookId=1
+    response = obj.get_all_pages(bookId)
+    return response
+
+@app.route("/ldaTopicsForBooks/<bookId1>/<bookId2>")
+@requires_auth
+def topTenLDATopics(bookId1=None, bookId2=None):
+    if not bookId1:
+        bookId1=1
+    if not bookId2:
+        bookId2=2
+    response = obj.get_Top_Ten_Topics(bookId1, bookId2)
+    return response
+
+@app.route("/commonLdaTopicsForBooks/<bookId1>/<bookId2>")
+@requires_auth
+def commonLdaTopicsForBooks(bookId1=None, bookId2=None):
+    if not bookId1:
+        bookId1=1
+    if not bookId2:
+        bookId2=2
+    response = obj.get_common_Topics(bookId1, bookId2)
+    return response
+
+@app.route("/allSentencesForTopic/<topic>")
+@requires_auth
+def allSentencesForTopic(topic=None):
+    response = obj.get_sentences_for_topic(topic)
+    return response
+
+@app.route("/allSentencesForTopicSortYear/<topic>")
+@requires_auth
+def allSentencesForTopicSortYear(topic=None):
+    response = obj.get_sentences_for_topic_sort_year(topic)
+    return response
+
+@app.route("/sentencesSimilarToTopic/<topic>")
+@requires_auth
+def sentencesSimilarToTopic(topic=None):
+    response = obj.get_similar_topic_sentences(topic)
+    return response
+
+@app.route("/sentencesPositiveToTopic/<topic>")
+@requires_auth
+def sentencesPositiveToTopic(topic=None):
+    response = obj.get_positive_topic_sentences(topic)
+    return response
+
+@app.route("/sentencesNegativeToTopic/<topic>")
+@requires_auth
+def sentencesNegativeToTopic(topic=None):
+    response = obj.get_negative_topic_sentences(topic)
+    return response
+
+@app.route("/bookDetailsForSentence/<sentenceId>")
+@requires_auth
+def bookDetailsForSentence(sentenceId=None):
+    response = obj.get_book_details(sentenceId)
+    return response
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
